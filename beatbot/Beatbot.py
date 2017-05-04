ee9aec7d66492d149c787163b9f7c83d5d8f04e6
Andrew Slotnick <andrew.slotnick@gmail.com>
2017-05-03 23:43:40 -0400

    analyze and plot spectral information

--- beatbot/Beatbot.py
+++ beatbot/Beatbot.py
@@ -1,10 +1,8 @@
 import numpy 
 from scipy.io import wavfile
-from scipy.signal import find_peaks_cwt
+from scipy import fftpack
 from matplotlib.mlab import specgram
 from matplotlib import pyplot
-
-ONSET_WIDTH = 2000
 
 class Beatbot(object):
     """
@@ -35,16 +33,29 @@ class Beatbot(object):
     @property
     def instrument_count(self):
         return 1
-    
 
-    def plot_onsets(self, path):
+
+    def plot(self, path):
         """
-        save a graph of the frequency with onsets 
+        plot the samples and onsets,
+        and the most important frequencies
         """
-        pyplot.plot(numpy.arange(self.samples.size), self.samples)
-        extent = numpy.absolute(self.samples).max()
-        pyplot.vlines(self.onsets, -extent, extent)
-        pyplot.axhline(self._threshold())
+        figure, axes = pyplot.subplots(3, sharex=True)
+
+        axes[0].plot(numpy.arange(self.samples.size), self.samples)
+        extent = numpy.absolute(self.samples).max()*1.25
+        axes[0].vlines(self.onsets, -extent, extent)
+        axes[0].axhline(self._threshold())
+
+        self._identify_frequency_ranges()
+        for z in range(len(self.dfts)):
+            onset, dft, frequencies = self.dfts[z]
+            onset_end = len(self.samples) if z == len(self.dfts)-1 else self.dfts[z+1][0]
+            onset_widths = [onset_end-onset for i in range(len(frequencies))]
+            axes[1].barh(frequencies, width=onset_widths, left=onset, height=5)
+
+        axes[2].specgram(self.samples, Fs=1)
+        
         pyplot.savefig(path)
 
 
@@ -68,7 +79,7 @@ class Beatbot(object):
         """
         identify the indices of the beginning of notes 
         (might correspond to the onset or the attack)
-        using a changes in amplitude
+        using a change in amplitude
         greater than a naive threshold
         """
         greater_mask = numpy.greater(numpy.diff(self.absolute_samples), self._threshold())
@@ -76,15 +87,41 @@ class Beatbot(object):
         self._onsets = greater_indices
 
 
-    def _combine_onsets(self):
+    def _identify_frequency_ranges(self, top=30, max_frequency=8000):
+        """
+        for each note (between two onsets),
+        perform FFT
+        retain only the top frequency magnitudes under the max_frequency
+        """
+        dfts = []
+        num_onsets = self.onsets.size
+        for o in range(num_onsets):
+            onset = self.onsets[o]
+            if o == num_onsets - 1:
+                next_onset = None
+            else:
+                next_onset = self.onsets[o+1]
+            note_samples = self.samples[onset:next_onset]
+            dft = numpy.absolute(fftpack.rfft(note_samples))
+            frequencies = fftpack.rfftfreq(note_samples.size, 1/self.rate)
+            max_frequency_index = numpy.argmax(frequencies>=max_frequency)
+            dft = dft[:max_frequency_index]
+            top_indices = numpy.argsort(dft)[-top:]
+            top_dft = dft[top_indices]
+            top_frequencies = frequencies[top_indices]
+            dfts.append((onset, top_dft, top_frequencies))
+        self.dfts = dfts
+
+
+    def _combine_onsets(self, resolution = 2000):
         """
         combine successive onsets that are fewer than
-        ONSET_WIDTH samples apart
+        resolution samples apart
         """
         current = self._onsets[0]
         new_onsets = [current]
         for candidate in self._onsets[1:]:
-            if candidate - current > ONSET_WIDTH:
+            if candidate - current > resolution:
                 new_onsets.append(candidate)
                 current = candidate
         self._onsets = numpy.array(new_onsets)
